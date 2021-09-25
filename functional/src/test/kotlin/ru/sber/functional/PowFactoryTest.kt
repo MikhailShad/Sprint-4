517a4134e398338f1acc5a9dd06505ce9c1b5939
Шадрин Михаил Дмитриевич <shadrin.m.dm@sberbank.ru>
2021-09-25 03:32:17 +0300

    Added solution for functional task

--- functional/src/test/kotlin/ru/sber/functional/PowFactoryTest.kt
+++ functional/src/test/kotlin/ru/sber/functional/PowFactoryTest.kt
@@ -1,13 +1,11 @@
 package ru.sber.functional
 
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-@Disabled
 class PowFactoryTest {
     @Test
     fun `buildPowFunction should return lambda It should calculate to second power`() {
-        // expect
-        // assertEquals(9, PowFactory.buildPowFunction(2)(3))
+        assertEquals(9, PowFactory.buildPowFunction(2)(3))
     }
 }
